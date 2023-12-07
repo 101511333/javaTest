b75c9fd681914d1e1c82c668b27e9d86e55b2b99
zzzxxxzzzcccc <151124366+zzzxxxzzzcccc@users.noreply.github.com>
2023-12-07 10:56:28 +0800

    Add files via upload

---
+++ mapper/LeavesMapper.java
@@ -0,0 +1,16 @@
+package com.example.springboot.mapper;
+
+import com.example.springboot.entity.Leaves;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2023-07-19
+ */
+public interface LeavesMapper extends BaseMapper<Leaves> {
+
+}
